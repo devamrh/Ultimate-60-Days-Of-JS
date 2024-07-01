4574d24553380aa59b17203caf1fe2d6ca11c16f
A. M. Rafinul Huq <116501394+devamrh@users.noreply.github.com>
2024-07-01 23:53:47 +0600

    Create first.js

---
+++ first.js
@@ -0,0 +1,11 @@
+// document.getElementByID().innerText
+console.log(document.getElementById('app-title').innerText);
+
+const defaultTitle = 'Cart';
+
+const prevTitle = document.getElementById('app-title').innerText = defaultTitle;
+
+
+// Get attributes
+
+console.log(document.getElementById('app-title').getAttribute);
